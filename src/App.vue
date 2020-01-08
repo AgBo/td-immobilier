12e11ff03c9d7686d92668671ff2982ca54a2265
unknown <aboitelle@hotmail.com>
2020-01-08 15:23:33 +0100

    etape 1

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,35 @@
 <template>
   <div id="app">
+    <BienAVendre v-for="bien in biens" :key="bien.id" :titre="bien.titre" :type="bien.type" :contact="bien.contact" :prix="bien.prix"></BienAVendre>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import BienAVendre from "./components/BienAVendre";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
+    BienAVendre
+  },
+
+  data() {
+    return {
+      biens: []
+    };
+  },
+  created() {
+    axios.get("http://localhost:3000/biens").then(resultat => {
+      this.biens = resultat.data;
+    });
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/BienAVendre.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+    {{titre}}
+    {{type}}
+    {{prix}}
+    {{contact}}
+</div>
+  
+</template>
+
+<script>
+export default {
+    name: 'BienAVendre',
+    //props: ['titre', 'type', 'prix', 'contact']
+    props:{
+        titre: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            required: true
+        },
+        prix: {
+            type: String,
+            required: true
+        },
+        contact: {
+            type: String,
+            required: true
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
